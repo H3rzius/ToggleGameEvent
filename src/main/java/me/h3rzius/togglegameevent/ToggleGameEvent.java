9de410841dd03fb39f72d8bf21d49389c5ec440c
H3rzius <h3rzius@gmail.com>
2023-04-24 14:44:24 +0200

    Adding project to git-repository

---
+++ src/main/java/me/h3rzius/togglegameevent/ToggleGameEvent.java
@@ -0,0 +1,17 @@
+package me.h3rzius.togglegameevent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ToggleGameEvent extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
